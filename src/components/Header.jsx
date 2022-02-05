dad8955ee0ce2dd01453605c8f2bca977948dd92
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-02-05 19:01:04 +0000

    update toggle theme button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ function Header({ themeToggle }) {
       <div className="header__container">
         <h4 className="header__title">Where in the world?</h4>
 
-        <div className="header__theme-toggle-wrapper" onClick={themeToggle}>
-          <button className="header__btn" type="button">
+        <div className="header__theme-toggle-wrapper">
+          <button className="header__btn" type="button" onClick={themeToggle}>
             <FaRegMoon className="moon-icon" /> Dark Mode
           </button>
         </div>
