1a27c01d6704437673509cd4fd6895db480df0f1
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-02-07 18:41:32 +0000

    add router link

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,17 +1,16 @@
 import { Link } from "react-router-dom";
-import CountryPage from "../pages/CountryPage";
 
 function Country({ name, population, region, capital, flags }) {
   return (
-    <Link to={`/${name.common}`} component={CountryPage}>
-      <div>
+    <>
+      <Link to={name.common}>
         <img src={flags.svg} alt={name.common} />
         <h4>{name.common}</h4>
         <p>Population: {population.toLocaleString("en-GB")}</p>
         <p>Region: {region}</p>
         <p>Capital: {capital}</p>
-      </div>
-    </Link>
+      </Link>
+    </>
   );
 }
 
