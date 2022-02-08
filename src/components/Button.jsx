99c0a0cef883146a499de33db48a0afcbee40d6c
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-02-08 18:30:21 +0000

    add generic button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+function Button({ children, onClick }) {
+  return (
+    <div>
+      <button type="button" onClick={onClick}>
+        {children}
+      </button>
+    </div>
+  );
+}
+
+export default Button;
