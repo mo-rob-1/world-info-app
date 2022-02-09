6b4e02ddbb47a4eb4fb7daa496bd92d9184d6171
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-02-09 19:10:15 +0000

    add link to header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import { BsMoonFill, BsMoon } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 function Header({ themeToggle, theme }) {
   return (
     <header>
       <div className="header__container">
-        <h4 className="header__title">Where in the world?</h4>
+        <Link to="/" className="header__title">
+          Where in the world?
+        </Link>
 
         <div className="header__theme-toggle-wrapper">
           <button className="header__btn" type="button" onClick={themeToggle}>
