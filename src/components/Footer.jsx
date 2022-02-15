9f6c3681abde2666a43b375f73be7f7a76f994bf
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-02-15 20:10:24 +0000

    add link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 function Footer() {
   return (
     <footer>
-      <small>Coded by Mo.</small>
+      <small>
+        Coded by <a href="https://mocodes.co.uk/">Mo.</a>
+      </small>
     </footer>
   );
 }
