67657a7d7259179fd4c99154f742fa2ad6283ac1
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-02-15 20:12:27 +0000

    add class names

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -3,13 +3,23 @@ import { Link } from "react-router-dom";
 function Country({ name, population, region, capital, flags }) {
   return (
     <>
-      <Link to={name.common}>
-        <img src={flags.png} alt={name.common} />
-        <h4>{name.common}</h4>
-        <p>Population: {population.toLocaleString("en-GB")}</p>
-        <p>Region: {region}</p>
-        <p>Capital: {capital}</p>
-      </Link>
+      <div className="card">
+        <Link className="card-link" to={name.common}>
+          <img src={flags.png} alt={name.common} />
+          <div className="card__info-wrapper">
+            <h3 className="card__country-name">{name.common}</h3>
+            <p className="card__population">
+              <strong>Population:</strong> {population.toLocaleString("en-GB")}
+            </p>
+            <p className="card__region">
+              <strong>Region:</strong> {region}
+            </p>
+            <p className="card__capital">
+              <strong>Capital:</strong> {capital}
+            </p>
+          </div>
+        </Link>
+      </div>
     </>
   );
 }
